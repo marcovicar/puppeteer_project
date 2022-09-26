627859f27e1585af18d9f9469974b50db3f3f17f
marcovicar <marcosvictormbi85@gmail.com>
2022-09-26 17:59:00 -0300

    configurado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        // headless: false
+    });
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
     await page.goto('https://github.com/marcovicar');
     await page.screenshot({ path: 'meugit.png' });
+
+    await browser.close();
 })();

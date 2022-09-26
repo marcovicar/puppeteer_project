63ad31a62f75f75594fd21d2cf79a6d3e3f099f3
marcovicar <marcosvictormbi85@gmail.com>
2022-09-26 16:46:06 -0300

    instalado e configurado o Puppeteer

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://github.com/marcovicar');
+    await page.screenshot({ path: 'meugit.png' });
+})();
